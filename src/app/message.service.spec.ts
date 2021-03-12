0892ac18f8a564aa2a50a3627fbdb003657ade68
Aditya Inapurapu <adityaii@gmail.com>
2021-03-11 22:19:19 -0800

    unit testing a service

---
+++ src/app/message.service.spec.ts
@@ -0,0 +1,21 @@
+import { TestBed } from '@angular/core/testing';
+
+import { MessageService } from './message.service';
+
+describe('MessageService', () => {
+  let service: MessageService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(MessageService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should add a message when add is called', () => {
+    service.add("Testing");
+    expect(service.messages).toHaveSize(1);
+  });
+});
